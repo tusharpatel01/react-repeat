d7e9e7602e0f6e21a028dcfd2f602916ba535f90
tusharpatel01 <jskobri6@gmail.com>
2024-09-23 08:46:04 +0530

    last commit

---
+++ firstproject/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>tushar patel with chai and code </h1>
+    </>
+  )
+}
+
+export default App
