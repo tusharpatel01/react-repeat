277f9b824653d23f30da627e543a403465d68ec4
tusharpatel01 <jskobri6@gmail.com>
2024-10-01 19:24:40 +0530

    count by me

---
+++ count/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+const increase=()=>{
+  setCount(count+1);
+  
+  }
+  const descrease=()=>{
+    setCount(count-1);
+}
+  return (
+    <>
+   <h1 
+   style={{
+    fontFamily:"cursive",
+    color:"white"
+   }}
+   >
+    counter app
+   </h1>
+<div style={{
+  display:"flex",
+  flexDirection:"column",
+  margin:"10px",
+  gap:"10px",
+  
+}}>
+  <button>
+    counter {count}
+  </button>
+  <button onClick={increase}>
+    
+    {`increase `}
+  </button>
+  <button onClick={descrease}>
+  {`  descrease `}
+  </button>
+</div>
+    </>
+  )
+}
+
+export default App
